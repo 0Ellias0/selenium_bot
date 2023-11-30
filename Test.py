11410a7c860f7b8571999fd0d637aad4fe935bd5
Hebert Palmera <hpalmerabuelvas@gmail.com>
2023-11-29 20:46:58 -0500

    set up  - new env with selenium framework

--- Test.py
+++ Test.py
@@ -1,1 +1,10 @@
-Test 
+# Selenium 
+from selenium import webdriver
+
+# PATH = r"C:\Users\hpalm\Downloads\chromedriver_win32\chromedriver.exe"
+driver = webdriver.Chrome()
+
+driver.get("https://github.com/0Ellias0")
+
+print(driver.title)
+
